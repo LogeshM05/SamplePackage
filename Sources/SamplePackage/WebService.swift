c051651e8fb5a4dfa35bd427d719567486201fe3
LogeshM05 <logeshwarlogs05@gmail.com>
2023-11-01 14:34:40 +0530

    Initial Commit

---
+++ Sources/SamplePackage/WebService.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  
+//
+//  Created by Logeshwar M on 31/10/23.
+//
+
+import Foundation
+
+public enum NetworkError: Error {
+    case badRequest
+    case decodingError
+}
+
+public class WebService {
+    
+    public init() { }
+    
+    public func fetch<T: Codable>(url: URL, parse: @escaping (Data) -> T?, completion: @escaping (Result<T?, NetworkError>) -> Void)  {
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil,
+                  (response as? HTTPURLResponse)?.statusCode == 200
+            else {
+                completion(.failure(.decodingError))
+                return
+            }
+            let result = parse(data)
+            completion(.success(result))
+            
+        }.resume()
+        
+    }
+    
+}
